8944ebc3087342bcc8cdd33b10a3b3373e7a07f7
AshaAbdu <halge87@yahoo.com>
2022-09-24 23:56:43 -0500

    week 12 final commit

--- src/test/java/TestDemoTest.java
+++ src/test/java/TestDemoTest.java
@@ -1,6 +1,8 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 import java.util.stream.Stream;
 
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.*;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TestDemoTest {
@@ -32,25 +35,29 @@ class TestDemoTest {
 	}
 	
 	static Stream<Arguments> argumentsForAddPositive() {
+		
 		//@formatter:off
 		return Stream.of(
 				
 				arguments(2, 4, 6, false),
-				arguments(0, 5, 6, true),
-				arguments(-1, 1, 0, true),
-				arguments(-1,-2, -4, true),
-				arguments(0, 0, 0, true),
-			
+				arguments(2, 5, 9, true),
+				arguments(-1, 6, 0, true),
+				arguments(-1,-6, -3, true)
 				);
 		//@formater:on
 	}
 	
-	@Test void assertThatNumberSquaredIsCorrect() {
-		TestDemo mockDemo = spy(testDemo);
-		doReturn(5).when(mockDemo).getRandomInt();
-		int fiveSquared = mockDemo.randomNumberSquared();
+
+	@Test 
+		void assertThatNumberSquaredIsCorrect() {
 		
-		assertThat(fiveSquared).isEqualTo(25);
-	}
+			TestDemo mockDemo = spy(testDemo);
+			
+			doReturn(5).when(mockDemo).getRandomInt();
+			
+			int fiveSquared = mockDemo.randomNumberSquared();
+		
+			assertThat(fiveSquared).isEqualTo(25);
+		}
 
 }
