dae1b9d2b3e85c2637e25e3e7ea18234dd07c5ab
AshaAbdu <halge87@yahoo.com>
2022-09-25 12:54:53 -0500

    modified

--- src/test/java/TestDemoTest.java
+++ src/test/java/TestDemoTest.java
@@ -1,6 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import static org.junit.jupiter.params.provider.Arguments.*;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TestDemoTest {
@@ -50,7 +49,7 @@ class TestDemoTest {
 
 	@Test 
 		void assertThatNumberSquaredIsCorrect() {
-		
+		  
 			TestDemo mockDemo = spy(testDemo);
 			
 			doReturn(5).when(mockDemo).getRandomInt();
